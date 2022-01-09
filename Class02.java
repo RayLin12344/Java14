388531362435fd02c0a86551e42beda0c10fef7c
RayLin12344 <93502719+RayLin12344@users.noreply.github.com>
2022-01-09 11:01:34 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,16 @@
+package Java14;
+import java.util.Scanner;
+import java.lang.String;
+public class Class02
+{
+   public static void main(String args[])
+   {
+      Scanner scn=new Scanner(System.in);
+      String str;
+
+      System.out.print("��J�r��:");
+      str=scn.nextLine();
+
+      System.out.println("�ഫ���j�g: "+str.toUpperCase());
+   }
+}
